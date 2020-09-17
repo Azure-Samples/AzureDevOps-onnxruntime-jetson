c2a28445a5480779c68bee4ff72691c179cc7dbf
manashgoswami <magoswam@microsoft.com>
2020-09-17 13:38:12 -0700

    Update voc_annotation.py

--- src/voc_annotation.py
+++ src/voc_annotation.py
@@ -1,7 +1,3 @@
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license. See LICENSE file in the project root for
-# full license information.
-
 import xml.etree.ElementTree as ET
 from os import getcwd
 
